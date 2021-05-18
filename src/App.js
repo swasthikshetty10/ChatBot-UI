de88bbdb87eca5f640878d92780dbf13f9b6b6c4
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-05-18 10:58:32 +0530

    message handling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React  from 'react';
 import Widget from "./Widget"
 function App() {
   return (
-    <div  className="App  flex justify-center items-center w-5/6">
+    <div  className="App ">
       <Widget style={{ height: '80vh' , width : "400px" }}  className = "h" />
      
     </div>
--- src/Widget/Chat.js
+++ src/Widget/Chat.js
@@ -6,11 +6,11 @@ import styled from 'styled-components'
 
 function Chat(props) {
     const [messages , setMessages] = useState([])
-    
+    // const [inputvalue , setInputvalue] = useState([])
     const MessageCard  = (props) => {
         console.log(props)
         return  props.sender ? <div className = " sender  flex items-end   mb-4 items-end justify-end">
-                    <div className = "flex flex-col space-y-2 text-xs max-w-xs  mx-2 items-start">
+                    <div className = "flex flex-col space-y-2 text-xs sm:text-s max-w-xs  mx-2 items-start">
                         <div><span className = "px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">{props.message}</span></div>
                     </div>
                         <img src= {props.url} alt="My profile" className = "w-6 h-6 rounded-full "/>
@@ -25,8 +25,10 @@ function Chat(props) {
     }
 
     const submitHandler = (value  ) => {
-        console.log(value)
-        setMessages([{ message : value , url : "https://picsum.photos/200/300", sender :" dasd"  } ,...messages])
+        if (value) {
+         setMessages([{ message : value , url : "https://picsum.photos/200/300", sender :" dasd"  } ,...messages]) 
+         value = null
+        }
     }
     return (
    
@@ -51,14 +53,14 @@ function Chat(props) {
         
        
 
-        <form className = "flex-shrink border-t-4 border-gray-200  p-3 " onSubmit = { (event) => {event.preventDefault(); submitHandler(event.target[1].value )  }}>  
+        <form className = "flex-shrink border-t-4 border-gray-200  p-3 " onSubmit = { (event) => {event.preventDefault(); submitHandler(event.target[1].value ); event.target[1].value =""  }}>  
         <div className = "relative flex">
                 <span className = "absolute inset-y-0 flex items-center">
                 <button type="button" className = "inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                         <EmojiHappyIcon className = "outline h-6 w-6 text-gray-500"/>
                     </button>
                 </span>
-                <input type="text" placeholder="Write Something" className = "w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-full py-2 sm:py-3" />
+                <input type="text" placeholder="Write Something"  className = "w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-full py-2 sm:py-3" />
                 <div className = "absolute right-0 items-center inset-y-0  sm:flex">
                     <button type="submit" className = "inline-flex items-center justify-center rounded-full h-10 w-10 sm:h-12 sm:w-12 transition duration-500 ease-in-out text-white bg-indigo-500 hover:bg-indigo-400 focus:outline-none">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className = "h-5 w-5 sm:h-6 sm:w-6 transform rotate-90">
